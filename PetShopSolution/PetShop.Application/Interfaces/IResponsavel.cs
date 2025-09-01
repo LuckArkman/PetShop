7399f9537a8d8c245ac19cdb43376e783cb7a4c8
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-09-01 20:30:26 -0300

    Removendo pastas backend indevidas do GitHub

--- PetShopSolution/PetShop.Application/Interfaces/IResponsavel.cs
+++
@@ -1,9 +0,0 @@
-using PetShop.Application.DTOs;
-
-namespace PetShop.Application.Interfaces;
-
-public interface IResponsavel
-{
-    Task<bool?> CheckPasswordAsync(string userPassword, string modelPassword);
-    Task<IEnumerable<string>> GetRolesAsync(MedicoVeterinario user);
-}
