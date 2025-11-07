6ce6e6119b7405b0c54ff292f3087731334139f0
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-11-07 15:40:11 -0300

    caixa

--- PetShopSolution/PetShop.API/Controllers/CaixaController.cs
+++ PetShopSolution/PetShop.API/Controllers/CaixaController.cs
@@ -15,7 +15,7 @@ public class CaixaController  : ControllerBase
     {
         _caixaService = caixaService;
     }
-
+    [HttpGet("GetByIdAsync/{id}")]
     public async Task<IActionResult> GetByIdAsync(string id, CancellationToken cancellationToken)
     {
         var pg = await _caixaService.GetById(id, cancellationToken);
@@ -23,6 +23,7 @@ public class CaixaController  : ControllerBase
         return Ok(pg);
 
     }
+    [HttpGet("GetPagamentosCompletosDoDiaAsync/{dataConsulta}")]
     public async Task<IActionResult> GetPagamentosCompletosDoDiaAsync(DateTime dataConsulta, CancellationToken cancellationToken)
     {
         var hoje = dataConsulta.Date;
@@ -30,6 +31,7 @@ public class CaixaController  : ControllerBase
         if (pg == null) return new NotFoundResult();
         return Ok(pg);
     }
+    [HttpGet("GetPagamentosPendentesDoDiaAsync/{dataConsulta}")]
     public async Task<IActionResult> GetPagamentosPendentesDoDiaAsync(DateTime dataConsulta, CancellationToken cancellationToken)
     {
         var hoje = dataConsulta.Date;
@@ -37,6 +39,7 @@ public class CaixaController  : ControllerBase
         if (pg == null) return new NotFoundResult();
         return Ok(pg);
     }
+    [HttpGet("GetPagamentosCanceladosDoDiaAsync/{dataConsulta}")]
     public async Task<IActionResult> GetPagamentosCanceladosDoDiaAsync(DateTime dataConsulta, CancellationToken cancellationToken)
     {
         var hoje = dataConsulta.Date;
@@ -44,24 +47,28 @@ public class CaixaController  : ControllerBase
         if (pg == null) return new NotFoundResult();
         return Ok(pg);
     }
+    [HttpGet("GetByClienteCpfAsync/{cpf}")]
     public async Task<IActionResult> GetByClienteCpfAsync(string cpf, CancellationToken cancellationToken)
     {
         var pg = await _caixaService.GetByCliente(cpf, cancellationToken);
         if (pg == null) return new NotFoundResult();
         return Ok(pg);
     }
+    [HttpPost("CreateAsync")]
     public async Task<IActionResult> CreateAsync(Pagamento pagamento, CancellationToken cancellationToken)
     {
         var pg = await _caixaService.Create(pagamento, cancellationToken);
         if (pg == null) return new NotFoundResult();
         return Ok(pg);
     }
+    [HttpPost("UpdateStatusAsync")]
     public async Task<IActionResult> UpdateStatusAsync(string id, PaidStatus status, CancellationToken cancellationToken)
     {
         var pg = await _caixaService.UpdateStatus(id, status, cancellationToken);
         if (pg == null) return new NotFoundResult();
         return Ok(pg);
     }
+    [HttpDelete("DeleteByDataHoraAsync/{dataHora}")]
     public async Task<IActionResult> DeleteByDataHoraAsync(DateTime dataHora, CancellationToken cancellationToken)
     {
         var hoje = dataHora.Date;
@@ -69,6 +76,7 @@ public class CaixaController  : ControllerBase
         if (pg) return new NotFoundResult();
         return Ok(pg);
     }
+    [HttpDelete("DeleteAsync/{id}")]
     public async Task<IActionResult> DeleteAsync(string id, CancellationToken cancellationToken)
     {
         var pg = await _caixaService.Delete(id, cancellationToken);
