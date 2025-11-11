40ac851e9e6a3186c77188e0d608e6a731001193
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2025-11-11 15:07:49 -0300

    up

--- PetShopSolution/PetShop.Application/Singletons/Singleton.cs
+++ PetShopSolution/PetShop.Application/Singletons/Singleton.cs
@@ -3,7 +3,7 @@ namespace PetShop.Application.Singletons;
 public class Singleton
 {
     static Singleton? _instance { get; set; }
-    public string src = "mongodb://petrakka:3702959@petshop_mongo:27017/";
+    public string src = "mongodb://petrakka:3702959@72.61.44.192:27017/";
     public static Singleton? Instance()
     {
         if (_instance == null)_instance = new Singleton();
